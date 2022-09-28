3207291f32eaf615e0fa6a823c730edbd2dcc59c
vivioss <viviovilh@gmail.com>
2022-09-28 21:44:34 +0300

    made classes

--- game-object/Stone.js
+++ game-object/Stone.js
@@ -1,27 +1,31 @@
-function Stone(radius, mass, color, speed, position) {
+class Stone {
 
-	this.radius = radius
-	this.mass = mass
-	this.color = color
-	this.speed = speed
-	this.position = position
+	constructor(radius, mass, color, speed, position) {
+		
+		this.radius = radius
+		this.mass = mass
+		this.color = color
+		this.speed = speed
+		this.position = position
 
-	const stoneElm = document.createElement("div")
-	stoneElm.style.width = 2 * this.radius + "px"
-	stoneElm.style.height = 2 * this.radius + "px"
-	stoneElm.style.borderRadius = '20000px'
-	stoneElm.style.backgroundColor = this.color
-	stoneElm.style.position = 'absolute'
-	stoneElm.style.top = this.position.y + "px"
-	stoneElm.style.left = this.position.x + "px"
-	this.elm = stoneElm
+		const stoneElm = document.createElement("div")
+		stoneElm.style.width = 2 * this.radius + "px"
+		stoneElm.style.height = 2 * this.radius + "px"
+		stoneElm.style.borderRadius = '20000px'
+		stoneElm.style.backgroundColor = this.color
+		stoneElm.style.position = 'absolute'
+		stoneElm.style.top = this.position.y + "px"
+		stoneElm.style.left = this.position.x + "px"
+		this.elm = stoneElm
 
-	document.body.appendChild(stoneElm)
-	
-	this.update = function() {
+		document.body.appendChild(stoneElm)
+	}
+
+	update() {
 
 		this.position.add(this.speed)
 		this.elm.style.top = this.position.y + "px"
 		this.elm.style.left = this.position.x + "px"
-	}	
-} 
+	}
+
+}
--- math/Vector2.js
+++ math/Vector2.js
@@ -1,10 +1,15 @@
-function Vector2(x = 0, y = 0) {
-	
-	this.x = x
-	this.y = y
+class Vector2 {
+  
+ 	constructor(x = 0, y = 0) {
 
-	this.add = function(vector2){
+		this.x = x
+		this.y = y
+ 	}
+
+	add(vector2) {
+
 		this.x = this.x + vector2.x
 		this.y = this.y + vector2.y
 	}
 }
+
